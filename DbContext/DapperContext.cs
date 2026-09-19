69cc6100a4d21babd933bc2fbe37f66b9c4cb57c
Mariela García Tejada <marielagarciatejada@gmail.com>
2026-09-19 15:18:03 -0400

    Creando la cadena de conexion con Dapper para la base de datos en Azure McdDb

--- DbContext/DapperContext.cs
+++ DbContext/DapperContext.cs
@@ -7,19 +7,31 @@ namespace MiWebApi.DbContext
 
         private readonly IConfiguration _configuracion;
         private readonly String _connectionString;
+        private readonly string _connectionStringHistorial;
 
         //Inyectar IConfiguration para leer el archivo de appsettings.json
         public DapperContext(IConfiguration configuracion)
         {
+
             _configuracion = configuracion;
-            // buscamos la cadena de conexion de somee 
-            _connectionString = _configuracion.GetConnectionString("SomeeConnection");
+            // Cadena de conexion de somee usada por ProductoService
+            _connectionString = _configuracion.GetConnectionString("SomeeConnection")
+                ?? throw new InvalidOperationException("No se encontró la cadena de conexión 'SomeeConnection' en appsettings.json"); ;
+
+            // Cadena de conexion de Azure sql usada por HistorialCalculoService
+            _connectionStringHistorial = _configuracion.GetConnectionString("AzureSQLConnection")
+                ?? throw new InvalidOperationException("No se encontró la cadena de conexión 'AzureSqlConnection' en appsettings.json");
+
         }
 
        /* metodo de crear y devolver la conexion a Sql Server
        este metodo es una interfaz de conexion que se puede conectar a cualquier base de datos */
         public IDbConnection CreateConnection()
         { return new SqlConnection(_connectionString); }
+
+        // conexion a la base de datos de azure sql usada para el historial de calculos de mcd
+        public IDbConnection CreateHistorialConnection()
+        { return new SqlConnection(_connectionStringHistorial); }
     }
 
 }
