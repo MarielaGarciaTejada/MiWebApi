38937a716130ab4e92a6fd03d72409846f9ee2ab
Mariela García Tejada <marielagarciatejada@gmail.com>
2026-09-20 21:04:42 -0400

    Cambio en la lectura de cadenas en DapperContext

--- DbContext/DapperContext.cs
+++ DbContext/DapperContext.cs
@@ -7,7 +7,7 @@ namespace MiWebApi.DbContext
 
         private readonly IConfiguration _configuracion;
         private readonly String _connectionString;
-        private readonly string _connectionStringHistorial;
+        private readonly String _connectionStringHistorial;
 
         //Inyectar IConfiguration para leer el archivo de appsettings.json
         public DapperContext(IConfiguration configuracion)
@@ -15,23 +15,29 @@ namespace MiWebApi.DbContext
 
             _configuracion = configuracion;
             // Cadena de conexion de somee usada por ProductoService
-            _connectionString = _configuracion.GetConnectionString("SomeeConnection")
-                ?? throw new InvalidOperationException("No se encontró la cadena de conexión 'SomeeConnection' en appsettings.json"); ;
+            _connectionString = _configuracion.GetConnectionString("SomeeConnection") ?? string.Empty;
 
             // Cadena de conexion de Azure sql usada por HistorialCalculoService
-            _connectionStringHistorial = _configuracion.GetConnectionString("AzureSQLConnection")
-                ?? throw new InvalidOperationException("No se encontró la cadena de conexión 'AzureSqlConnection' en appsettings.json");
+            _connectionStringHistorial = _configuracion.GetConnectionString("AzureSQLConnection");
 
         }
 
        /* metodo de crear y devolver la conexion a Sql Server
        este metodo es una interfaz de conexion que se puede conectar a cualquier base de datos */
         public IDbConnection CreateConnection()
-        { return new SqlConnection(_connectionString); }
+        {
+            if (string.IsNullOrEmpty(_connectionString))
+                throw new InvalidOperationException("No se configuró la cadena de conexión 'SomeeConnection'.");
+            return new SqlConnection(_connectionString); 
+        }
 
         // conexion a la base de datos de azure sql usada para el historial de calculos de mcd
         public IDbConnection CreateHistorialConnection()
-        { return new SqlConnection(_connectionStringHistorial); }
+        {
+            if (string.IsNullOrEmpty(_connectionStringHistorial))
+                throw new InvalidOperationException("No se configuró la cadena de conexión 'AzureSQLConnection'.");
+            return new SqlConnection(_connectionStringHistorial); 
+        }
     }
 
 }
